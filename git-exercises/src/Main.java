9560257e59d1be8e9c55b0067e38a69950e3d60a
claus0200 <claus.peter.joergensen@gmail.com>
2024-02-01 10:58:11 +0100

    2nd try

--- git-exercises/src/Main.java
+++ git-exercises/src/Main.java
@@ -1,35 +1,16 @@
-<<<<<<< HEAD
 public class Main {
-    public static void main(String[] args) {
-        for (int i = 1; i < 101; i++) {
-=======
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-    // then press Enter. You can now see whitespace characters in your code.
-    public class Main {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 100; i++) {
-
->>>>>>> d4d3a5ae11bdec9c9a0640900b231407bf9860f6
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
-            }
-            else if (i % 3 == 0) {
+            } else if (i % 3 == 0) {
                 System.out.println("Fizz");
-            }
-            else if (i % 5 == 0) {
+            } else if (i % 5 == 0) {
                 System.out.println("Buzz");
-<<<<<<< HEAD
-            }
-            else {
+            } else {
                 System.out.println(i);
             }
-=======
-            }else {
-                System.out.println(i);
-            }
-
-            }
->>>>>>> d4d3a5ae11bdec9c9a0640900b231407bf9860f6
         }
     }
+}
