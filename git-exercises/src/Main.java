7a2e8b2a0909816f53abf4d8cf0093769db3ead1
Benjamin <bennobarrales@gmail.com>
2024-02-01 12:23:09 +0100

    Last

--- git-exercises/src/Main.java
+++ git-exercises/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         for (int i = 1; i <= 200; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("BuzzFizz");
+                System.out.println("FizzBuzz");
             } else if (i % 3 == 0) {
                 System.out.println("Fizz");
             } else if (i % 5 == 0) {
