e9b0b5d428ce3e5d42a9995e4597e27ede42eda9
Benjamin <bennobarrales@gmail.com>
2024-02-01 11:46:20 +0100

    BuzzFizz

--- git-exercises/src/Main.java
+++ git-exercises/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         for (int i = 1; i <= 200; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
+                System.out.println("BuzzFizz");
             } else if (i % 3 == 0) {
                 System.out.println("Fizz");
             } else if (i % 5 == 0) {
