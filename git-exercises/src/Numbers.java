bf2d211b7e5ffb909a5b6d4958f1359e5e7b95e4
FerdinandAV <ferdinandsjskole@gmail.com>
2024-02-01 11:22:19 +0100

    First commit

--- git-exercises/src/Numbers.java
+++ git-exercises/src/Numbers.java
@@ -1,5 +1,5 @@
 public class Numbers {
-    public void numbersOneToOnehundred() {
+    public void numberOneToOnehundred() {
         for (int i = 0; i < 100; i++) {
             if (i % 3 == 0) {
                 System.out.println("Fizz");
@@ -8,9 +8,7 @@ public class Numbers {
                 System.out.println("Buzz");
             }
             if (i % 3 * i == 0 && i % 5 * i == 0) {
-                System.out.println("FizzBuzz");
-            } else {
-                System.out.println(i);
+                System.out.println("Buzz");
             }
         }
     }
