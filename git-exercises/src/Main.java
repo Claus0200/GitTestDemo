086ad7e90f15b9ec80f5856fa5ae573138ddcbd0
cphmk <mkhallberg@hotmail.com>
2024-02-01 11:17:44 +0100

    featureMahdi

--- git-exercises/src/Main.java
+++ git-exercises/src/Main.java
@@ -2,16 +2,20 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 100; i++) {
+            if( i % 15 == 0) {
+                System.out.println("FizzBuzz");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            } else if(i % 3  == 0) {
+                System.out.println("Fizz");
+
+            } else if(i % 5 == 0) {
+                System.out.println("Buzz");
+
+            } else { System.out.println(i);
+
+            }
         }
     }
 }
