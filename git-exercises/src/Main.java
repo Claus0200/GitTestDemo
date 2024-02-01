3cbd6661f2fbc0fd0ca7ccddaf54dc1664e93fc1
Benjamin <bennobarrales@gmail.com>
2024-02-01 12:11:55 +0100

    Stash Appky

--- git-exercises/src/Main.java
+++ git-exercises/src/Main.java
@@ -12,5 +12,14 @@ public class Main {
                 System.out.println(i);
             }
         }
+        System.out.println("");
+        printNumbers(1,1000);
+
+    }
+    public static void printNumbers(int start, int end){
+
+        for(int j = start; j <= end; j++){
+            System.out.println(j);
+        }
     }
 }
